9678dd857c62773811921cb7dbbcc03761ac9374
Dan Rees <dan.rees@circleci.com>
2022-12-20 10:43:53 -0500

    fix: provide better base unit test
    This still isn't a great test but it should allow for "a test" without blocking
    the thread with a running server by running main

--- main.go
+++ main.go
@@ -5,16 +5,22 @@ import (
 	"os"
 
 	"github.com/danrees/twitter-clone/router"
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-
+func setup() (string, *gin.Engine) {
 	addr, ok := os.LookupEnv("SERVER_ADDR")
 	if !ok {
 		addr = ":8080"
 	}
 
 	r := router.New()
+	return addr, r
+}
+
+func main() {
+
+	addr, r := setup()
 	log.Printf("Starting server on %v", addr)
 	if err := r.Run(addr); err != nil {
 		log.Fatal(err)
--- main_test.go
+++ main_test.go
@@ -1,19 +1,29 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func Test_main(t *testing.T) {
+	"gotest.tools/assert"
+)
+
+func Test_setup(t *testing.T) {
 	tests := []struct {
 		name string
+		want string
+		//want1 *gin.Engine
 	}{
 		// TODO: Add test cases.
 		{
-			name: "do tests",
+			"basic test",
+			":8080",
+			//router.New(),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			main()
+			got, _ := setup()
+			assert.Equal(t, tt.want, got)
+			//assert.Equal(t, tt.want1, got1)
 		})
 	}
 }
